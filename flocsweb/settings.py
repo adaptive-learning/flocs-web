27f0e4856321035ca98dfe6e760842bc59407ef3
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-12-21 14:33:13 +0100

    Database settings

--- flocsweb/settings.py
+++ flocsweb/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -86,13 +87,9 @@ WSGI_APPLICATION = 'flocsweb.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
+    "default": dj_database_url.config(default='sqlite:///' +
+                                      os.path.join(BASE_DIR, 'db.sqlite3'))
 }
 
 
