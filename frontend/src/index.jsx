0467e073c48f3c4344b231a216b605b450c8004e
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-02-16 20:39:37 +0100

    Upgrade flocs dependencies

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux';
 import routes from './config/routes'
 import reducers from './reducers'
-import createLogger from 'redux-logger'
-import promise from 'redux-promise-middleware'
 import { Router, browserHistory } from 'react-router'
 import { FlocsProvider } from 'flocs-visual-components';
 
@@ -16,18 +11,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 // axios config
 import './config/axiosConfig'
 
-const logger = createLogger();
-
-const store = createStore(reducers, applyMiddleware(promise(), thunk, logger));
-
 const app = (
-  <Provider store={store}>
-    <FlocsProvider>
-      <Router history={browserHistory}>
-        {routes}
-      </Router>
-    </FlocsProvider>
-  </Provider>
+  <FlocsProvider reducers={reducers}>
+    <Router history={browserHistory}>
+      {routes}
+    </Router>
+  </FlocsProvider>
 );
 
 const mountElement = document.getElementById('flocsApp')
--- frontend/src/reducers/index.jsx
+++ frontend/src/reducers/index.jsx
@@ -1,17 +1,14 @@
-import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import { flocsComponentsReducer } from 'flocs-visual-components';
 import { practiceReducer } from './practiceReducer';
 import { taskSessionReducer } from './taskSessionReducer';
 import { tasksReducer } from './tasksReducer';
 
-const reducers = combineReducers({
+const reducers = {
   tasks: tasksReducer,
   taskSession: taskSessionReducer,
   practice: practiceReducer,
-  flocsComponents: flocsComponentsReducer,
   routing: routerReducer
-});
+};
 
 export default reducers;
 
