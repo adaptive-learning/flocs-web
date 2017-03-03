f03396ada63583fd4cc143903b7dca3f804c8a92
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2017-03-03 12:43:33 +0100

    Remove task-preview route

--- frontend/src/config/routes.jsx
+++ frontend/src/config/routes.jsx
@@ -5,14 +5,12 @@ import Main from '../components/Main';
 import Home from '../components/Home';
 import PracticePage from '../pages/PracticePage';
 import TasksTableContainer from '../containers/TasksTableContainer';
-import TaskPreviewContainer from '../containers/TaskPreviewContainer';
 
 const routes = (
     <Route path='/' component={Main}>
         <IndexRoute component={Home} />
         <Route path='/tasks' component={TasksTableContainer} />
         <Route path='/task/:taskId' component={PracticePage} />
-        <Route path='/task-preview/:taskId' component={TaskPreviewContainer} />
         <Route path='/task-editor' component={TaskEditorContainer} />
     </Route>
 );
